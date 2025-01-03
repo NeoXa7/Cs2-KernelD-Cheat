f256c6dca70f0373c674e7c96f9a5329c72daf88
Neo7 <164035951+NeoXa7@users.noreply.github.com>
2025-01-03 17:30:02 +0545

    Update CView.cpp

--- Delta-Cs2/SDK/Maths/CView.cpp
+++ Delta-Cs2/SDK/Maths/CView.cpp
@@ -7,14 +7,12 @@ Bool CView::Project3DWorldTo2D(const Vector3& worldPos, Vector2& screenPos, cons
 	Float clip_z = worldPos.x * viewMatrix.matrix[2][0] + worldPos.y * viewMatrix.matrix[2][1] + worldPos.z * viewMatrix.matrix[2][2] + viewMatrix.matrix[2][3];
 	Float clip_w = worldPos.x * viewMatrix.matrix[3][0] + worldPos.y * viewMatrix.matrix[3][1] + worldPos.z * viewMatrix.matrix[3][2] + viewMatrix.matrix[3][3];
 
-	if (clip_w < 0.1f) // To prevent division by zero and culling objects behind the camera
+	if (clip_w < 0.1f) 
 		return false;
 
-	// Normalize to NDC (Normalized Device Coordinates)
 	Float ndc_x = clip_x / clip_w;
 	Float ndc_y = clip_y / clip_w;
 
-	// Convert to screen coordinates
 	screenPos.x = (SCREEN_WIDTH / 2 * ndc_x) + (ndc_x + SCREEN_WIDTH / 2);
 	screenPos.y = -(SCREEN_HEIGHT / 2 * ndc_y) + (ndc_y + SCREEN_HEIGHT / 2);
 
